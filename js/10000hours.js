99e96b0b6277549d935c73727b49e607f98bed0f
hb_kim <binikim66@gmail.com>
2022-09-18 12:24:01 +0900

    모달창 열고 닫는 기능 구현

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,16 @@
-// js file
+const openModal = document.querySelector(".modal-open");
+const modal = document.querySelector(".cont-modal");
+const closeModal = document.querySelector(".close-modal");
+
+
+function displayModal() {
+    modal.style.display = "flex";
+}
+
+function hiddenModal() {
+    modal.style.display = "none";
+}
+
+
+openModal.addEventListener("click", displayModal);
+closeModal.addEventListener("click", hiddenModal);
